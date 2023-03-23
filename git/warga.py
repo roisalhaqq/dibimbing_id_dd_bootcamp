bf686a9b1a7e17de46dbc9ba13ec7af6dea47352
roisalhaqqgoto <rois.alhaqq@tokopedia.com>
2023-03-23 11:21:38 +0700

    add class warga

---
+++ git/warga.py
@@ -0,0 +1,7 @@
+class Warga():
+    def __init__(self, NIK: str):
+        self.nik = NIK
+
+    def nyoblos(self, pilihan: str):
+        self.pilihan = pilihan
+        print(f"{self.nik} memilih {pilihan}")
